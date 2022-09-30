4f25ac86fbd4c52e344266033bbf3fc73664f7a8
Alex Clayden <alexclayden@gmail.com>
2022-09-30 16:50:04 +0100

    published

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 setup(
   name="quakerheritage",
-  version="0.0.1",
+  version="0.0.2",
   description="",
   long_description=README,
   long_description_content_type="text/markdown",
   author="",
   author_email="",
   license="AGPL",
-  packages=["quakerheritage"],
+  packages=["src/quakerheritage"],
   zip_safe=False
 )
