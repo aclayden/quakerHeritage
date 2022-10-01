c5be12210ed36dbac401092f8127abb791fb693d
Alex Clayden <alexclayden@gmail.com>
2022-10-01 08:54:33 +0100

    bug fix

--- build/lib/quakerheritage/build.py
+++ build/lib/quakerheritage/build.py
@@ -2,24 +2,21 @@
 
 """Unifies the functional modules and runs the main code to create a formatted Pandas DataFrame from pdfs held by Britain Yearly Meeting.
 
-This module runs automatically when opened, in concert with the other modules in this package. Please note, the import structure of the local files is designed only to work in CLI. 
-You will receive a ModuleNotFoundError if you attempt to run this code in IDE. Replace lines 19 and 20 with the following to work locally in IDE:
-    import getWebData as gwd
-    import cleanseData as cd
+This module runs automatically when opened, in concert with the other modules in this package. 
 
 This module contains the following functions:
 
 - `getOnlineData(url)` - Collects pdfs from webpage, extracts text to dictionary, creates DataFrame from all dicitonaries and hygienes data.
-- `main()` - executes automatically to run the entire project
+
 """
-print(__package__, __name__)
+
 import tkinter as tk
 from tkinter import filedialog
 
 import pandas as pd
 
-from quakerheritage.utils import getWebData as gwd
-from quakerheritage.utils import cleanseData as cd
+import getWebData as gwd
+import cleanseData as cd
 
 url = "https://heritage.quaker.org.uk/"
 
@@ -34,19 +31,17 @@ def getOnlineData(url: str) -> pd.DataFrame:
     """
     pdfList = gwd.getUrls(url)
     dictList = []
-    for pdf in pdfList[1:3]: 
+    for pdf in pdfList: 
         dictList.append(gwd.pdfDataExtract(pdf))
     df = cd.createDataFrame(dictList)
     df = cd.hygieneDataFrame(df)
     return df
+    
 
-def main():
+if __name__ == '__main__':
     root = tk.Tk()
     root.withdraw()
 
     file_path = filedialog.askdirectory() + '\quakerHeritageDB.csv'
     db = getOnlineData(url)
     cd.saveToCSV(db, file_path)
-
-if __name__ == '__main__':
-    main()
--- quakerheritage/__init__.py
+++ quakerheritage/__init__.py
@@ -3,11 +3,11 @@
 
 Modules exported by this package:
 
-`callFunctions`: Unifies the functional modules and runs the main code to create a formatted Pandas DataFrame from pdfs held by Britain Yearly Meeting\n
+`build`: Unifies the functional modules and runs the main code to create a formatted Pandas DataFrame from pdfs held by Britain Yearly Meeting\n
 
 `getWebData`: Main web function that collects url data and extracts text from each pdf examined\n
 
 `cleanseData`: Runs Pandas functionality to merge text data into workable DataFrame and creates consistent data quality\n
 
 """
-__version__ = "0.0.4"
+__version__ = "0.0.5"
