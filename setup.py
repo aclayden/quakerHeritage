07bbedc2e531365082c24227cc257d7935f79471
Nicolas Gonzalez <ndgfrs@gmail.com>
2022-10-05 23:59:54 -0400

    Update to better include project dependencies.

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 import pathlib
-from setuptools import setup
-HERE = pathlib.Path(__file__).parent
+import setuptools
+
+HERE = pathlib.Path(__file__).parent.replace("\\", "/")
 README = (HERE / "README.md").read_text()
-setup(
-  name="quakerheritage",
-  version="1.0.1",
-  description="",
-  long_description=README,
-  long_description_content_type="text/markdown",
-  author="",
-  author_email="",
-  license="AGPL",
-  packages=["quakerheritage"],
-  zip_safe=False
-)
+
+requirements = []
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name="quakerheritage",
+        version="1.0.1",
+        description="",
+        long_description=README,
+        long_description_content_type="text/markdown",
+        author="",
+        author_email="",
+        license="AGPL",
+        packages=["quakerheritage"],
+        zip_safe=False,
+        requires=requirements
+    )
